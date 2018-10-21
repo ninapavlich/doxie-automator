ebacc08283898d5c76aa3d5c8e40e1cd04aaf1e1
Nina Pavlich <nina@ninalp.com>
2018-10-20 23:17:48 -0700

    getting files ready for pypi

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup
 setup(
     name='doxieautomator',    # This is the name of your PyPI-package.
     version='0.2',                          # Update the version number for new releases
-    scripts=['doxieautomator']                  # The name of your scipt, and also the command you'll be using for calling it
+    scripts=['doxieautomator'],                 # The name of your scipt, and also the command you'll be using for calling it
+    url='https://github.com/ninapavlich/doxie-automator',
+    author='Nina Pavlich',
+    author_email='nina@ninalp.com',
+    license = "MIT",
+    include_package_data = True
 )
